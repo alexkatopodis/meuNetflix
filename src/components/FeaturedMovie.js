db4d664812ba576ede4e0299f6361e91262d45c3
Alex Katopodis <alexkatopodis@live.com>
2021-05-09 15:28:22 -0300

    Corrigido CSS do featured--buttons

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -27,19 +27,20 @@ export default ({item}) => {
                             <div className='featured--year'>{firstDate.getFullYear()}</div>
                             <div className='featured--seasons'>{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}
                             </div> 
-                            <div className='featured--description'>{item.overview}
-                            <div className='featured-buttons'>
-                                <a href={`/watch/${item.id}`}className='featured-watchbutton'>▶ Assistir</a>
-                                <a href={`/list/add/${item.id}`}className='featured-mylistbutton'>+ Minha Lista</a>
+                            <div className='featured--description'>{item.overview}</div> 
+                            <div className='featured--buttons'>
+
+                                <a href={`/watch/${item.id}`}className='featured--watchbutton'>▶ Assistir</a>
+                                <a href={`/list/add/${item.id}`}className='featured--mylistbutton'>+ Minha Lista</a>
 
                             </div>
                             <div className='featured--genres'><strong>Gêneros:</strong> {genres.join(', ')}</div>
-                                </div>       
+                                      
 
-                            </div>
-                            </div>
                         </div>
                         </div>
+                    </div>
+                </div>
                               
             
         </section>
